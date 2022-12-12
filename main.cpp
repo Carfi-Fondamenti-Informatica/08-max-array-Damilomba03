0bc0500cf6f673ed5faeeae6f0748346a10a596e
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-12-12 20:21:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+
+    int x;
+    cin>>x;
+    float a[x];
+    float y;
+    for(int i=0; i<x; i++){
+        cin>>y;
+        a[i]=y;
+    }
+    cout<<max(a,y);
+
+    return 0;
 }
